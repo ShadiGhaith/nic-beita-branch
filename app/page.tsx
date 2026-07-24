7cc008011d8cde6f19d115f8249f5f039afc06e0
ShadiGhaith <shadibusiness15@gmail.com>
2026-07-24 12:27:56 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -214,7 +214,7 @@ export default function NICPalestineLanding() {
             </div>
             <style jsx>{`
               @keyframes marquee {
-            50% { transform: translateX(-150%); }
+            50% { transform: translateX(-80%); }
                 100% { transform: translateX(100%); }
               }
               .animate-marquee {
